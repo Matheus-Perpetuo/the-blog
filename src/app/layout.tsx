491a61814ce0661f090f89c64e206cf4f552ee19
Matheus Richard <matheusperpetuo@gmail.com>
2025-09-02 20:59:25 -0300

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "The blog - Este e um blog com next.js",
+  description: "Essa seria a descrição dessa página",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode
+};
+
+export default function RootLayout({children,}: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="pt-BR">
+      <body className="qualquer">
+        <header><h1>Header</h1></header>
+
+        <div className="bg-red-500">{children}</div>
+
+        <footer><h1>Footer</h1></footer>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return <h1>Ola de dentro de page.tsx</h1>
+}
+
+// page.tsx = index.html
+// app/page.tsx -> /
+// app/about/page.tsx -> /about
